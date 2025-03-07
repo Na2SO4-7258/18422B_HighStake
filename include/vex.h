817b2bf60c8eb860e995d8046113781d4f5a6e12
Lunyan Wang <lwan0096@uni.sydney.edu.au>
2025-03-07 20:51:56 +1100

    ver2

--- include/vex.h
+++ include/vex.h
@@ -14,7 +14,7 @@
 #define opcolor (auto_select%2==0?'R':'B')
 
 #define pi 3.1415926
-#define wheel_r 2.54
-#define wheel_s 2.54
+#define wheel_r 2.54  //轮半径
+#define wheel_s 2.54  
 #define Ss 12      //侧轮到中心距离
 #define Sr 0.5     //竖轮到中心距离
